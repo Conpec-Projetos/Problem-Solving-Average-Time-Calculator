57cb21a49cbf46f42b02bc5bdc9c34b2c80f41bb
yellowisk <oheitoralmeida2013@gmail.com>
2025-01-27 13:59:18 -0300

    rm extra print

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ problem_ages = ProblemSolverReader(
     "data/mock_problems.csv"
     ).get_problem_ages_with_curr_date_column()
 
-print("Idades dos problemas:", problem_ages)
-
 solver_efficiency = ProblemSolverEfficiency(problem_ages)
 solver_efficiency.display_results()
